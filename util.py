f54805dd8832e952746b50e7cb1371da848e15a6
Ming <shimeming@gmail.com>
2024-03-14 01:21:03 +0800

    Add cbc_print to util

--- util.py
+++ util.py
@@ -1,7 +1,16 @@
-import os, platform
+import os
+import platform
+
 
 def clear():
     if platform.system() == 'Windows':
         os.system('cls')
     else:
         os.system('clear')
+
+
+def cbc_print(string: str, interval: float):
+    '''
+    print the string char by char
+    '''
+    print(string)
