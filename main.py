2527003bb1382a24f0e3688416266403bf757a31
Ming <shimeming@gmail.com>
2024-03-14 01:01:04 +0800

    Add welcome message

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import os, platform
+import util
+import const
+
+def main():
+    util.clear()
+    # Big, Blocks, Chiseled, Modular, Slant, Standard, Train, ANSI-, ivrit, Larry 3D, Rammstein, speed, X992
+    print()
+    print(const.WELCOME_MESSAGE)
+    input("Press enter to continue...")
+    util.clear()
+
+
+if __name__ == '__main__':
+    main()
