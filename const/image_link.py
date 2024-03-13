bb8d6cf892facec790c1abf8a656e8338912167f
Ming <shimeming@gmail.com>
2024-03-14 02:08:26 +0800

    Add some mock image links

---
+++ const/image_link.py
@@ -0,0 +1,3 @@
+GITHUB_COMMIT_HIS_BUTTON_POSITION = 'https://t4.ftcdn.net/jpg/05/80/61/41/360_F_580614175_LW6mydM9ii6KEwPjF8aAlPqmlj21YKiC.jpg'
+GITHUB_BRANCH_LIST_BUTTON_POSITION = 'https://media.istockphoto.com/id/1280378750/photo/cute-striped-cat-sitting-in-the-autumn-garden-surrounded-by-golden-and-yellow-fallen-leaves.jpg?s=612x612&w=0&k=20&c=hjz8Blb46IPRJicwhXi2W12T8y6f5pHyaHDfZgnoNbE='
+GITHUB_FILE_BOOKMARK_POSITION = 'https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg'
